d48b4951b89b75384ff094fb734048e6ccdab259
Alonso-pixel <kreitos_05@outlook.com>
2024-04-03 16:32:46 -0600

    link to stripe

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 function Buy() {
   return (
     <div className="flex flex-col items-center">
       <div className="text-2xl">Comprar artículo</div>
-      <Button className="w-fit bg-green-600">Comprar</Button>
+      <Link href={"https://stripe.dev/elements-examples/"}>
+        <Button className="w-fit bg-green-600">Comprar</Button>
+      </Link>
     </div>
   );
 }
